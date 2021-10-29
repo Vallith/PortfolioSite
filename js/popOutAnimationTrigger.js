aa89e96361bfe7997a6250e4a9d3675e09e95eae
Jack Allpike <jack@allpikes.com.au>
2021-10-29 14:50:47 +0800

    initial commit

---
+++ js/popOutAnimationTrigger.js
@@ -0,0 +1,48 @@
+
+window.onload = Setup;
+
+let topLeft;
+let bottomLeft;
+let middleRight;
+
+let leftArrowBody;
+let leftArrowHead;
+
+let rightArrowBody;
+let rightArrowHead;
+
+let aboutMeArrow;
+
+function Setup() {
+
+    console.log("Running Setup...");
+    topLeft = document.getElementsByClassName("topLeftPopOut")[0];
+    bottomLeft = document.getElementsByClassName("bottomLeftPopOut")[0];
+    middleRight = document.getElementsByClassName("middleRightPopOut")[0];
+
+    leftArrowBody = document.getElementsByClassName("arrowBody")[0];
+    leftArrowHead = document.getElementsByClassName("arrowHead")[0];
+
+    rightArrowBody = document.getElementsByClassName("arrowBody")[1];
+    rightArrowHead = document.getElementsByClassName("arrowHead")[1];
+
+    aboutMeArrow = document.getElementsByClassName("arrowScroll")[0];
+
+    document.getElementsByClassName("arrowScroll")[0].addEventListener("click", TriggerAnimation);
+
+}
+
+function TriggerAnimation() {
+
+    /* Animations are paused by default via css */
+    topLeft.style.animationPlayState = "running";
+    bottomLeft.style.animationPlayState = "running";
+    middleRight.style.animationPlayState = "running";
+
+    leftArrowBody.style.animationPlayState = "running";
+    leftArrowHead.style.animationPlayState = "running";
+
+    rightArrowBody.style.animationPlayState = "running";
+    rightArrowHead.style.animationPlayState = "running";
+
+}
